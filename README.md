3aa9641f6f3ca1a9c1978699c5937859c03950bd
eqpoqpe <eqpoqpe@gmail.com>
2022-05-01 17:05:09 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# vue-kyte58
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/vue-kyte58)
---
+++ src/App.vue
@@ -0,0 +1,42 @@
+<script>
+import AppUtil from './App.util';
+
+export default {
+  name: 'App',
+  components: {},
+  methods: {
+    plusOne() {},
+  },
+  mounted() {},
+  ...AppUtil(),
+};
+</script>
+
+<template>
+  <div id="app">
+    <h1>Component Effect Hub</h1>
+    <p>Copyright (c) 2022 <span class="profile">Vu Coolgifts</span></p>
+  </div>
+</template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+#app p {
+  color: black;
+  font-style: italic;
+  font-size: 15px;
+}
+
+.profile {
+  background-color: black;
+  color: white;
+}
+</style>
